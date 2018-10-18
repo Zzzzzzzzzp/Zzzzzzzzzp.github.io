1ca6fbbfa3b3cea8d124bbad796eb93c63433e06
Zzzp <2099529795@qq.com>
2018-10-18 12:36:20 +0800

    hehe

--- mkz/data/init.php
+++ mkz/data/init.php
@@ -1,5 +1,5 @@
 <?php
-  $conn=mysqli_connect("192.168.1.11","root","","mkz",3306);
+  $conn=mysqli_connect("127.0.0.1","root","","mkz",3306);
   $sql="SET NAMES UTF8";
   mysqli_query($conn,$sql);
 ?>
